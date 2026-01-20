fadef04ab6bce513fd1cd27b0e05630d0bb6fdb3
Vlad Bystrov <vbystrov@cinemalab.io>
2026-01-20 19:56:50 +0300

    bump versions

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object Dependencies {
   // versions
-  lazy val scalatestVersion = "3.2.16"
+  lazy val scalatestVersion = "3.2.19"
 
   // libraries
   val scalatest = "org.scalatest" %% "scalatest" % scalatestVersion % Test
